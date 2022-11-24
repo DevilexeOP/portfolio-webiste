5dab6b9716164157b5e9b3c94dda052aba828564
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2022-11-24 19:04:58 +0530

    new files 24 nov

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,55 @@ import React, { useState } from "react";
 import "../Styles/contact.css";
 
 const Contact = (props) => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  // connect to firebase
+  const submitData = async (event) => {
+    event.preventDefault();
+    const { name, email, subject, message } = userData;
+    if (name && email && subject && message) {
+      const res = await fetch(
+        "https://portfolio-new-2311b-default-rtdb.asia-southeast1.firebasedatabase.app//contactDataBase.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            subject,
+            message,
+          }),
+        }
+      );
+      if (res) {
+        setUserData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+        });
+        alert("Message sent");
+      } else {
+        alert("Error 404");
+      }
+    } else {
+      alert("Error 404 , Message cannot be empty");
+    }
+  };
   return (
     <React.Fragment>
       <section id="contact">
@@ -48,22 +97,29 @@ const Contact = (props) => {
           data-aos-easing="ease-in-sine"
           data-aos-delay="1500"
           data-aos-offset="0"
+          method="POST"
         >
           <div className="row">
             <div className="col-6">
               <input
                 type="text"
+                name="name"
                 placeholder="Name"
                 className="form-control"
                 id="name"
+                value={userData.name}
+                onChange={postUserData}
               />
             </div>
             <div className="col-3">
               <input
                 type="email"
+                name="email"
                 placeholder="Email"
                 id="email"
                 className="form-control"
+                value={userData.email}
+                onChange={postUserData}
               />
             </div>
 
@@ -71,16 +127,25 @@ const Contact = (props) => {
               <input
                 type="subject"
                 placeholder="Subject"
+                name="subject"
                 id="subject"
                 className="form-control"
+                value={userData.subject}
+                onChange={postUserData}
               />
             </div>
 
-            <textarea name="textarea" id="textArea">
+            <textarea
+              placeholder="Message"
+              name="message"
+              id="textarea"
+              value={userData.message}
+              onChange={postUserData}
+            >
               Message
             </textarea>
           </div>
-          <button type="submit" id="button">
+          <button type="submit" id="button" onClick={submitData}>
             Send Message
           </button>
         </form>
